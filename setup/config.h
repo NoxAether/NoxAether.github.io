4abb5d89c9f3b34fc63b00cfd6fa2af54b6b4b3e
NoxAether <107390370+NoxAether@users.noreply.github.com>
2025-11-27 16:51:04 +1100

    init

---
+++ setup/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define SCREEN_WIDTH    500
+#define SCREEN_HEIGHT   500
+#define SCREEN_FPS      60
+#define SCREEN_TITLE    "Raiders test"
+
+
+#endif // CONFIG_H_
